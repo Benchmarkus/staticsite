2c3dcdabdd5efc72a38ec1365342e1387f983a22
Benchmarkus <toyryla.markus@gmail.com>
2025-06-16 16:48:56 +0300

    refac

--- src/testconversion.py
+++ src/testconversion.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType
 from blocktype import BlockType
-from main import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, markdown_to_blocks, block_to_block_type
+from conversion import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, markdown_to_blocks, block_to_block_type
 
 class TestMain(unittest.TestCase):
     def test_eq_text(self):
