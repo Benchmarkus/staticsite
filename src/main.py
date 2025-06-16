fd48978a8f57a54a0b4da0927d6c741728befd2a
Benchmarkus <toyryla.markus@gmail.com>
2025-06-16 17:42:00 +0300

    static to public file/dir copying

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import os
+import shutil
 
 def main():
-    copy_from_directory_to()
+    clear_source("public")
+    copy_from_directory_to("static", "public")
 
+def clear_source(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+        os.mkdir(directory)
 
-def copy_from_directory_to():
-    from_path = os.path("static/")
-    to_path = os.path("public/")
-
-    
-
+def copy_from_directory_to(source, destination):
+    os.makedirs(destination, exist_ok=True)
+    for item in os.listdir(source):
+        s = os.path.join(source, item)
+        d = os.path.join(destination, item)
+        if os.path.isdir(s):
+            copy_from_directory_to(s, d)
+        else:
+            shutil.copy(s, d)
 
 main()
